480cd916cb5dddc1397dec38fe98e0901126e210
noxayyyy <aisuru.nox@gmail.com>
2026-03-13 18:34:57 +0500

    starter

---
+++ app/src/main/java/com/example/androiduitesting/City.java
@@ -0,0 +1,20 @@
+package com.example.androiduitesting;
+
+public class City {
+    private String city;
+    private String province;
+
+    City(String city, String province){
+        this.city = city;
+        this.province = province;
+    }
+
+    String getCityName(){
+        return this.city;
+    }
+
+    String getProvinceName(){
+        return this.province;
+    }
+}
+https://github.com/natewu/cmput301-lab7-demo
