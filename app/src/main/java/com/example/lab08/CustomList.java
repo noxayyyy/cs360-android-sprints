684e6aaf5241fa015797df4ab5b84d915103fa9c
noxayyyy <aisuru.nox@gmail.com>
2026-03-19 15:58:16 +0500

    init

---
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -0,0 +1,21 @@
+package com.example.lab08;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomList {
+    private List<City> cities;
+
+    public CustomList() {
+        this.cities = new ArrayList<>();
+    }
+
+    public void addCity(City city) {
+        cities.add(city);
+    }
+
+    
+
+    // Will be implemented later using TDD (leave blank for now)
+    // public boolean hasCity(City city) { ... }
+}
